263e4e48a4bf5810a086c5ee4726c0c2fcac3ff8
arbuzPR304 <k.kalfas@po.edu.pl>
2016-05-08 10:02:20 -0700

    first commit

--- planApp/admin.py
+++ planApp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+
+admin.site.register(Lectures)
+admin.site.register(Teachers)
+admin.site.register(Teacherworkingplan)
+admin.site.register(Room)
+admin.site.register(Lecturetypes)
+admin.site.register(Lecturefrequenceparity)
+admin.site.register(Groups)
+admin.site.register(Departments)
+admin.site.register(Daysofweek)
+admin.site.register(Lecturerooms)
+admin.site.register(Person)
+
+
+
+
--- planApp/models.py
+++ planApp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Daysofweek(models.Model):
+    idofweek = models.AutoField(db_column='idOfWeek', primary_key=True)  # Field name made lowercase.
     dayofweekname = models.CharField(db_column='dayOfWeekName', max_length=45)  # Field name made lowercase.
     dayofweekshortname = models.CharField(db_column='dayOfWeekShortName', max_length=45)  # Field name made lowercase.
 
@@ -13,6 +14,7 @@ class Daysofweek(models.Model):
 
 
 class Departments(models.Model):
+    idofdepartment = models.AutoField(db_column='idOfDepartment', primary_key=True)  # Field name made lowercase.
     departmentname = models.CharField(db_column='departmentName', max_length=120)  # Field name made lowercase.
 
     class Meta:
@@ -20,25 +22,18 @@ class Departments(models.Model):
         db_table = 'departments'
 
 
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
 class Groups(models.Model):
+    idgroup = models.AutoField(db_column='idGroup', primary_key=True)  # Field name made lowercase.
     groupparentid = models.IntegerField(db_column='groupParentId')  # Field name made lowercase.
     groupname = models.CharField(db_column='groupName', max_length=120)  # Field name made lowercase.
     groupsemester = models.IntegerField(db_column='groupSemester')  # Field name made lowercase.
-    groupdepartmentid = models.ForeignKey(Departments, models.DO_NOTHING, db_column='groupDepartmentId')  # Field name made lowercase.
+    groupprofileid = models.ForeignKey('Profiles', db_column='groupProfileId')  # Field name made lowercase.
+    groupdepartmentid = models.ForeignKey(Departments, db_column='groupDepartmentId')  # Field name made lowercase.
 
     class Meta:
-        managed = False
+        managed = True
         db_table = 'groups'
+        unique_together = ['idgroup', 'groupdepartmentid', 'groupprofileid']
 
 
 class Lecturefrequenceparity(models.Model):
@@ -50,12 +45,62 @@ class Lecturefrequenceparity(models.Model):
         db_table = 'lectureFrequenceParity'
 
 
+class Lectureleaders(models.Model):
+    idlectureleaders = models.AutoField(db_column='idLectureLeaders', primary_key=True)  # Field name made lowercase.
+    teacherid = models.ForeignKey('Teachers', db_column='teacherId')  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'lectureLeaders'
+        unique_together = ['idlectureleaders', 'teacherid']
+
+
+class Lecturerooms(models.Model):
+    idofrooms = models.AutoField(db_column='idOfRooms', primary_key=True)  # Field name made lowercase.
+    roomid = models.ForeignKey('Room', db_column='roomId')  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'lectureRooms'
+        unique_together = ['idofrooms', 'roomid']
+
+
 class Lecturetypes(models.Model):
+    idoflecturetype = models.AutoField(db_column='idOfLectureType', primary_key=True)  # Field name made lowercase.
     lecturename = models.CharField(db_column='lectureName', max_length=120)  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'lectureTypes'
+
+
+class Lectures(models.Model):
+    idoflectures = models.AutoField(db_column='idOfLectures', primary_key=True)  # Field name made lowercase.
+    lectureleadersid = models.ForeignKey(Lectureleaders, db_column='lectureLeadersId')  # Field name made lowercase.
+    lecturedayofweekid = models.ForeignKey(Daysofweek, db_column='lectureDayOfWeekId')  # Field name made lowercase.
+    lecturename = models.CharField(db_column='lectureName', max_length=120)  # Field name made lowercase.
+    lecturetypeid = models.ForeignKey(Lecturetypes, db_column='lectureTypeId')  # Field name made lowercase.
+    lecturegroupid = models.ForeignKey(Groups, db_column='lectureGroupId')  # Field name made lowercase.
+    lecturefrequence = models.IntegerField(db_column='lectureFrequence')  # Field name made lowercase.
+    lecutrefrequenceparityid = models.ForeignKey(Lecturefrequenceparity, db_column='lecutreFrequenceParityId')  # Field name made lowercase.
+    lectureroomsid = models.ForeignKey(Lecturerooms, db_column='lectureRoomsId')  # Field name made lowercase.
+    lecturetimestart = models.TimeField(db_column='lectureTimeStart')  # Field name made lowercase.
+    lecturetimestop = models.TimeField(db_column='lectureTimeStop')  # Field name made lowercase.
+    lectureannotiation = models.CharField(db_column='lectureAnnotiation', max_length=45)  # Field name made lowercase.
+
+    class Meta:
+        managed = True
+        db_table = 'lectures'
+        unique_together = ['idoflectures', 'lecturedayofweekid', 'lecturegroupid', 'lectureleadersid', 'lecturetypeid', 'lecutrefrequenceparityid', 'lecutrefrequenceparityid']
+
+
+class Profiles(models.Model):
+    idofprofile = models.AutoField(db_column='idOfProfile', primary_key=True)  # Field name made lowercase.
+    profilename = models.CharField(db_column='profileName', max_length=120)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'profiles'
 
 
 class Room(models.Model):
@@ -68,10 +113,20 @@ class Room(models.Model):
         db_table = 'room'
 
 
+class Teacherworkingplan(models.Model):
+    teacherid = models.ForeignKey('Teachers', db_column='teacherId', primary_key=True)  # Field name made lowercase.
+    dayofweekid = models.ForeignKey(Daysofweek, db_column='dayOfWeekId')  # Field name made lowercase.
+    timestart = models.TimeField(db_column='timeStart')  # Field name made lowercase.
+    timestop = models.TimeField(db_column='timeStop')  # Field name made lowercase.
 
+    class Meta:
+        managed = True
+        db_table = 'teacherWorkingPlan'
+        unique_together = ['teacherid', 'dayofweekid']
 
 
 class Teachers(models.Model):
+    idofteacher = models.AutoField(db_column='idOfTeacher', primary_key=True)  # Field name made lowercase.
     title = models.CharField(max_length=45)
     name = models.CharField(max_length=45)
     surname = models.CharField(max_length=250)
@@ -81,33 +136,6 @@ class Teachers(models.Model):
         managed = False
         db_table = 'teachers'
 
-
-class Teacherworkingplan(models.Model):
-    teacherid = models.ForeignKey(Teachers, models.DO_NOTHING, db_column='teacherId')  # Field name made lowercase.
-    dayofweekid = models.ForeignKey(Daysofweek, models.DO_NOTHING, db_column='dayOfWeekId')  # Field name made lowercase.
-    timestart = models.TimeField(db_column='timeStart')  # Field name made lowercase.
-    timestop = models.TimeField(db_column='timeStop')  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'teacherWorkingPlan'
-
-class Lectures(models.Model):
-    lectureleadersid = models.ManyToManyField(Teachers, db_column='lectureleadersid')
-    lecturedayofweekid = models.ForeignKey(Daysofweek, models.DO_NOTHING, db_column='lectureDayOfWeekId')  # Field name made lowercase.
-    lecturetypeid = models.ForeignKey(Lecturetypes, models.DO_NOTHING, db_column='lectureTypeId')  # Field name made lowercase.
-    lecturegroupid = models.ForeignKey(Groups, models.DO_NOTHING, db_column='lectureGroupId')  # Field name made lowercase.
-    lecutrefrequenceparityid = models.ForeignKey(Lecturefrequenceparity, models.DO_NOTHING, db_column='lecutreFrequenceParityId')  # Field name made lowercase.
-    lectureroomsid = models.ManyToManyField(Room, db_column='lectureroomsid')
-
-    lecturename = models.CharField(db_column='lectureName', max_length=120)  # Field name made lowercase.
-    lecturefrequence = models.IntegerField(db_column='lectureFrequence')  # Field name made lowercase.
-    lecturetimestart = models.TimeField(db_column='lectureTimeStart')  # Field name made lowercase.
-    lecturetimestop = models.TimeField(db_column='lectureTimeStop')  # Field name made lowercase.
-    lectureannotiation = models.CharField(db_column='lectureAnnotiation', max_length=45)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'lectures'
-
-
+class Person(models.Model):
+    title = models.CharField(max_length=45)
+    name = models.CharField(max_length=45)
